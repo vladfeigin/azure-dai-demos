a7f490611bd5ad1c73b7be372d73d179ee976a92
“vladfeigin” <vladfeigin@microsoft.com>
2024-10-22 15:22:26 +0300

    adding RAG integration with aimodel

--- llmops/aimodel/ai_model.py
+++ llmops/aimodel/ai_model.py
@@ -6,39 +6,54 @@ import os
 load_dotenv()
 
 from azure.identity import DefaultAzureCredential
-from langchain_openai import AzureOpenAI
+from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import PromptTemplate
 
-from aisearch.ai_search import search
+#from aisearch.ai_search import search
 
 #This model is a wrapper on top of Azue Open AI 
 #It used for assistant to answer an end user question
 #It utilize Lanchain framework 
 
  #init the azure active directory credential in order to generate the token
-credential = DefaultAzureCredential()
+#credential = DefaultAzureCredential()
 # Set the API type to `azure_ad`
-os.environ["OPENAI_API_TYPE"] = "azure_ad"
+#os.environ["OPENAI_API_TYPE"] = "azure_ad"
 # Set the API_KEY to the token from the Azure credential
-os.environ["OPENAI_API_KEY"] = credential.get_token("https://cognitiveservices.azure.com/.default").token    
+#api_key = credential.get_token("https://cognitiveservices.azure.com/.default").token 
+#os.environ["OPENAI_API_KEY"] = api_key
     
 
 class AIModel:
    
     
-    def __init__(self, azure_deployment, openai_api_version, azure_endpoint):
-        self.llm = AzureOpenAI(  
+    def __init__(self, azure_deployment, openai_api_version, azure_endpoint, api_key):
+        self.llm = AzureChatOpenAI(  
             azure_deployment=azure_deployment,
             openai_api_version=openai_api_version,
             azure_endpoint=azure_endpoint,
-            #api_key=api_key,
+            api_key=api_key,
+            temperature=0
+            
         )
+        #self.llm.invoke("What is Microsoft Azure?")
     
-    def __set_system_prompt__():
+    def generate_response(self, prompt):
+        response = self.llm.invoke(prompt)
+        return response 
+   
              
-
-
 if __name__ == "__main__":
+    
     # Initialize the AI model
-    llm = AIModel()
+    azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT")
+    openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION")
+    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+    api_key=os.getenv("AZURE_OPENAI_KEY")
+    
+    llm = AIModel(azure_deployment,openai_api_version,azure_endpoint,api_key)
+    response = llm.generate_response(formatted_prompt)
+    
+  
 
     
--- llmops/rag/rag.py
+++ llmops/rag/rag.py
@@ -11,18 +11,47 @@ load_dotenv()
 from aisearch.ai_search import search
 from aimodel.ai_model import AIModel
 
+from langchain_core.prompts import HumanMessagePromptTemplate
+from langchain_core.prompts import SystemMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+
 #init Azure open ai env variables
+
+SYSTEM_PROMPT="You are helpful assistant, helping the use nswer questions about Microsoft technologies. \
+    You answer questions about Azure, Microsoft 365, Dynamics 365, Power Platform, Azure, Microsoft Fabric and other Microsoft technologies \
+        Don't use your internal knowledge, but only provided context in the prompt. \
+        Don't answer not related to Microsoft technologies questions. \
+        Provide the best answer based on the context in concise and clear manner. \
+        Find the main points in a question and emphasize them in the answer. "
+        
+HUMAN_TEMPLATE=""" context: {context},  question: {question}"""
+
 
 class RAG:
     def __init__(self) -> None:
-        self.llm = AIModel(
+        self.aimodel = AIModel(
+            
             azure_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT"),
             openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
             azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+            api_key=os.getenv("AZURE_OPENAI_KEY")
         )
-
+        self.system_prompt_template = SystemMessagePromptTemplate.from_template(SYSTEM_PROMPT)
+        self.human_promot_template = HumanMessagePromptTemplate.from_template(HUMAN_TEMPLATE)
+        self.chat_prompt_template = ChatPromptTemplate.from_messages([SYSTEM_PROMPT, HUMAN_TEMPLATE])
+    
+    def answer(self, question, context, **kwargs):
+        prompt = self.chat_prompt_template.format_prompt(context=context, question=question)
+        print (f" final prompt= {prompt}")
+        response = self.aimodel.generate_response(prompt)
+        return response.content
+        
 
 if __name__ == "__main__":
     # Initialize the RAG model
+    
     rag = RAG()
-    r("What is the capital of France?")
+    resp = rag.answer(question="What's Microsoft Fabric?", context="")
+    print (f"***response= {resp}")
+
+    
